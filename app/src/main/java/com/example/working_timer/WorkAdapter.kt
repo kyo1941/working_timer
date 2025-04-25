d08c949ebc49ad0d02e540401ca91e8268d10554
kyo1941 <kyo1941@outlook.jp>
2025-04-25 23:27:18 +0900

    feat: ボタンを追加

--- app/src/main/java/com/example/working_timer/WorkAdapter.kt
+++ app/src/main/java/com/example/working_timer/WorkAdapter.kt
@@ -12,12 +12,12 @@ class WorkAdapter(private var workList: List<Work>) :
     inner class WorkViewHolder(private val binding: ItemWorkBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(work: Work) {
-            binding.startTime.text = "開始: ${work.start_time}"
-            binding.endTime.text = "終了: ${work.end_time}"
+            binding.startTime.text = "開始  ${work.start_time}"
+            binding.endTime.text = "終了  ${work.end_time}"
             val h = work.elapsed_time / 3600
             val m = (work.elapsed_time % 3600) / 60
             val s = work.elapsed_time % 60
-            binding.elapsedTime.text = "活動時間: %02d:%02d:%02d".format(h, m, s)
+            binding.elapsedTime.text = "活動時間  %02d:%02d:%02d".format(h, m, s)
         }
     }
 

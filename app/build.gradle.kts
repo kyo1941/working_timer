421decd4c40a1d188ea95d0e6fecd6cec5075b69
kyo1941 <kyo1941@outlook.jp>
2025-04-27 04:22:17 +0900

    feat: 指定範囲の合計活動時間を計算する

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,5 +62,5 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.1.0")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material:material-icons-extended:1.5.3")
-
+    implementation("com.google.android.material:material:1.10.0")
 }

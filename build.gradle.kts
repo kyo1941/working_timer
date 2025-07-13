6ee5633cc1b4fe45c75e4931cf6228622119cd91
kyo1941 <kyo1941@outlook.jp>
2025-07-14 04:09:27 +0900

    feat: 依存関係の追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.ksp.gradle.plugin) apply false
+    alias(libs.plugins.compose.compiler) apply false
+
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
 }

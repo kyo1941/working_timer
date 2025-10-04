b18c1c1a951bf55e5f4d58dca0b40bda43704ea3
kyo1941 <kyo1941@outlook.jp>
2025-10-04 23:52:21 +0900

    fix:　ダイアログの実装ファイルの位置を移動

--- app/src/main/java/com/example/working_timer/ui/components/MyAlertDialog.kt
+++ app/src/main/java/com/example/working_timer/ui/components/MyAlertDialog.kt
@@ -1,4 +1,4 @@
-package com.example.working_timer.component
+package com.example.working_timer.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
